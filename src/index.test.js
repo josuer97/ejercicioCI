44ccc666338e84c4780059b95b21f21b5ac0f3ab
josué <besteujt@gmail.com>
2019-06-17 17:39:32 +0100

    test failed 2

--- src/index.test.js
+++ src/index.test.js
@@ -4,8 +4,8 @@ test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(5);
+test('adds failed', () => {
+    expect(sum(2, 2)).toBe(5);
 });
 
 
