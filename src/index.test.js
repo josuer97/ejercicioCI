690e23c1415e0dca5943a25dad3e36d17f2d815b
josué <besteujt@gmail.com>
2019-06-17 17:36:24 +0100

    test failed

--- src/index.test.js
+++ src/index.test.js
@@ -3,3 +3,9 @@ const sum = require('./index');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(5);
+});
+
+
