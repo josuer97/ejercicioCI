2f1df023dd5dff57e60cfc468957c9fd103aed64
josué <besteujt@gmail.com>
2019-06-17 20:31:22 +0100

    asd

--- src/server.js
+++ src/server.js
@@ -1,13 +1,11 @@
-
-var express = require('express');
-var app = express();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('<iframe src="https://giphy.com/embed/Efpfk11XWXwTSKqgV2" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(PORT, function () {
+    console.log('Server is running, port ' + PORT);
 });
-
-
